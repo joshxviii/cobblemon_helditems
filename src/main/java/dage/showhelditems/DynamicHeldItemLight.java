26628e88720429b7f1af9aac1fc0d683a2844c75
Josh <125091065+joshxviii@users.noreply.github.com>
2025-01-22 21:29:57 -0500

    added back lamb dynamic light support

---
+++ src/main/java/dage/showhelditems/DynamicHeldItemLight.java
@@ -0,0 +1,16 @@
+package dage.showhelditems;
+
+import dev.lambdaurora.lambdynlights.api.DynamicLightsInitializer;
+import dev.lambdaurora.lambdynlights.api.item.ItemLightSourceManager;
+
+/**
+ * @author Josh
+ */
+public class DynamicHeldItemLight implements DynamicLightsInitializer {
+    public static final String MOD_ID = "show-held-items";
+
+    @Override
+    public void onInitializeDynamicLights(ItemLightSourceManager itemLightSourceManager) {
+
+    }
+}
